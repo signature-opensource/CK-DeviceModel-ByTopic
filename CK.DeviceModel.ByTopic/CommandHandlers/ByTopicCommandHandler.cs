f1359e48e0ec09c4e76ef63af5e55de47767a73f
Arthur Cheng <arthur.cheng@signature.one>
2025-02-18 13:50:02 +0100

    fixed command handlers result creation, added IncomingValidator to ensure ISetTopicMultiColorCommand.Colors is not empty and added comments

--- CK.DeviceModel.ByTopic/CommandHandlers/ByTopicCommandHandler.cs
+++ CK.DeviceModel.ByTopic/CommandHandlers/ByTopicCommandHandler.cs
@@ -12,57 +12,51 @@ namespace CK.DeviceModel.ByTopic;
 public class ByTopicCommandHandler : IAutoService
 {
     readonly IEnumerable<ITopicAwareDeviceHost> _hosts;
-    private readonly ScopedUserMessageCollector _collector;
-    readonly PocoDirectory _pocoDirectory;
+    readonly ScopedUserMessageCollector _collector;
 
-    public ByTopicCommandHandler( IEnumerable<ITopicAwareDeviceHost> hosts, ScopedUserMessageCollector collector, PocoDirectory pocoDirectory )
+    public ByTopicCommandHandler( IEnumerable<ITopicAwareDeviceHost> hosts, ScopedUserMessageCollector collector )
     {
         _hosts = hosts;
         _collector = collector;
-        _pocoDirectory = pocoDirectory;
-    }
-
-    IEnumerable<ITopicAwareDeviceHost> ForDeviceFullName( string? deviceHostName )
-    {
-        if( string.IsNullOrWhiteSpace( deviceHostName ) ) return _hosts;
-        if( deviceHostName == "*" ) return _hosts;
-
-        return _hosts.Where( x => x.DeviceHostName.StartsWith( deviceHostName ) );
     }
 
     [CommandHandler]
     public async Task<ISetTopicCommandResult> HandleTurnOnTopicCommandAsync( IActivityMonitor monitor, ISetTopicColorCommand cmd )
     {
         await HandleDeviceTopicCommandAsync( monitor, cmd );
+        return cmd.CreateResult<ISetTopicCommandResult>( c => c.SetUserMessages( _collector ) );
+    }
 
-        //TODO: See why we cant use CreateResult
-        return _pocoDirectory.Create<ISetTopicCommandResult>( r =>
+    [IncomingValidator]
+    public void ValidateMultipleTopicsCommand( IActivityMonitor m, UserMessageCollector collector, ISetTopicMultiColorCommand cmd )
+    {
+        using( m.OpenInfo( "Validating ISetTopicMultiColorCommand." ) )
         {
-            r.Success = _collector.ErrorCount == 0;
-            r.UserMessages.AddRange( _collector.UserMessages );
-        } );
+            if( !cmd.Colors.Any() )
+            {
+                collector.Error( "Colors collection must not be empty." );
+            }
+        }
     }
 
     [CommandHandler]
     public async Task<ISetTopicCommandResult> HandleTurnOnMultipleTopicsCommandAsync( IActivityMonitor monitor, ISetTopicMultiColorCommand cmd )
     {
         await HandleDeviceTopicCommandAsync( monitor, cmd );
-
-        //TODO: See why we cant use CreateResult
-        return _pocoDirectory.Create<ISetTopicCommandResult>( r =>
-        {
-            r.Success = _collector.ErrorCount == 0;
-            r.UserMessages.AddRange( _collector.UserMessages );
-        } );
-
+        return cmd.CreateResult<ISetTopicCommandResult>( c => c.SetUserMessages( _collector ) );
     }
 
-    private async Task HandleDeviceTopicCommandAsync( IActivityMonitor monitor, ICommandDeviceTopics cmd )
+    async Task HandleDeviceTopicCommandAsync( IActivityMonitor monitor, ICommandDeviceTopics cmd )
     {
         foreach( var topic in cmd.Topics )
         {
             var deviceHostName = topic.Split( "/" )[0];
-            var targets = ForDeviceFullName( deviceHostName );
+            var targets = _hosts;
+            if( !string.IsNullOrWhiteSpace( deviceHostName ) || deviceHostName != "*" )
+            {
+                targets =_hosts.Where( x => x.DeviceHostName.StartsWith( deviceHostName ) );
+            }
+
             if( targets.Any() )
             {
                 foreach( var host in targets )
@@ -74,8 +68,6 @@ public class ByTopicCommandHandler : IAutoService
             {
                 _collector.Error( $"{deviceHostName} not found in hosts" );
             }
-
         }
-
     }
 }
--- CK.IO.DeviceModel.ByTopic/Commands/ISetTopicColorCommand.cs
+++ CK.IO.DeviceModel.ByTopic/Commands/ISetTopicColorCommand.cs
@@ -1,11 +1,15 @@
 using CK.Cris;
 using CK.DeviceModel;
 
-namespace CK.IO.DeviceModel.ByTopic
-{
-    public interface ISetTopicColorCommand : ICommandDeviceTopics, ICommand<ISetTopicCommandResult>
-    {
-        StandardColor Color { get; set; }
-    }
+namespace CK.IO.DeviceModel.ByTopic;
 
+/// <summary>
+/// Cris command that will be dispatched to the targeted device hosts, based on the given topics.
+/// </summary>
+public interface ISetTopicColorCommand : ICommandDeviceTopics, ICommand<ISetTopicCommandResult>
+{
+    /// <summary>
+    /// The desired <see cref="StandardColor"/> to turn targeted topics with.
+    /// </summary>
+    StandardColor Color { get; set; }
 }
